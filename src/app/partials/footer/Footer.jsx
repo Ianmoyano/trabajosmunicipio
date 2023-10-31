09e6ebe600514d1379040f829e88a723191167d8
JuanSuarez1 <juansuarez60249@gmail.com>
2023-10-31 19:58:18 -0300

    Footer completado

--- src/app/partials/footer/Footer.jsx
+++ src/app/partials/footer/Footer.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Footer = () => {
     return(
-        <div>Footer</div>
+        <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados </footer>
     )
 }
