7ae11de751ffee283e4dd9f1de1ae50be84ed1d2
JuanSuarez1 <juansuarez60249@gmail.com>
2023-11-04 18:17:32 -0300

    cambio masivo

--- src/app/partials/footer/Footer.jsx
+++ src/app/partials/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export const Footer = () => {
     return(
-        <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados </footer>
+        <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados
+        <img src="./img/bahiaescudo.png"  /></footer>
     )
 }
--- src/app/partials/main/Main.jsx
+++ src/app/partials/main/Main.jsx
@@ -4,24 +4,47 @@ export const Main = () => {
   return (
     <>
       <br />
-      <h1 class="infociudad">.Bahía Blanca es una ciudad que se fundo en 1828.</h1>
-      <h1 class="infociudad">.Actualmente tiene una cantidad de 335.190 habitantes.</h1>
-      <h1 class="infociudad">.Está ubicada en el sudoeste de la provincia de Buenos Aires, <br></br>
-        en el centro-este de la Argentina</h1>
+      <li class="infociudad">Bahía Blanca es una ciudad que se fundo en 1828.</li>
+      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
+      <li class="infociudad">Está ubicada en el sudoeste de la provincia de Buenos Aires, <br></br>
+        en el centro-este de la Argentina</li>
+      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
+      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
+      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
 
-      <br></br>
-  <img src="./img/bahiablanca.png" alt="Geo ubicación de el partido de Bahía blanca"  width="500" height="400"/>
+      <div class="rectangulo">.</div>
+      <img class="imagen1" src="./img/bahiablanca.png" alt="Geo ubicación de el partido de Bahía blanca" />
 
 
-      <h1 class="infociudad">Con esta pagina web de la municipalidad de Bahía Blanca buscamos<br></br>
-        que las personas que necesiten un trabajo puedan hacerlo mediante<br></br>
-        nosotros. Haciendo, de esta manera, que la gente pueda encontrar<br></br>
-        un trabajo que se adapte a sus necesidades.</h1>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./img/bahia1.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/bahia2.jpeg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="./img/bahia3.jpg" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
-<button class="boton">Empleos disponibles</button>
+      <div class="empleos">Con esta pagina web de la municipalidad de Bahía Blanca buscamos que las personas que necesiten un trabajo puedan hacerlo mediante nosotros. Porque VOS importas, para conocer más apreta el siguiente botón</div>
+
+
+
+      <button class="boton">Empleos disponibles</button>
 
     </>
 
   )
 }
-export default Main
