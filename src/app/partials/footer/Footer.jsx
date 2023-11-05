9cc031a91f5109eddf1ef6c6a947c72d6d5e21ef
Ianmoyano <moyanoianlucca@gmail.com>
2023-11-05 00:54:37 -0300

    Main casi listo

--- src/app/partials/footer/Footer.jsx
+++ src/app/partials/footer/Footer.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export const Footer = () => {
     return(
         <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados
-        <img src="./img/bahiaescudo.png"  /></footer>
+        <img class="escudo" src="./img/bahiaescudo.png"  /></footer>
     )
 }
--- src/app/partials/main/Main.jsx
+++ src/app/partials/main/Main.jsx
@@ -4,43 +4,21 @@ export const Main = () => {
   return (
     <>
       <br />
-      <li class="infociudad">Bahía Blanca es una ciudad que se fundo en 1828.</li>
-      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
-      <li class="infociudad">Está ubicada en el sudoeste de la provincia de Buenos Aires, <br></br>
-      ㅤ en el centro-este de la Argentina</li>
-      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
-      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
-      <li class="infociudad">Actualmente tiene una cantidad de 335.190 habitantes.</li>
+      <p class="infociudad"><div class="rectangulo3"> ㅤ</div><div class="rectangulo2"></div>Bahía Blanca es una ciudad que se fundo en 1828.
+        <br></br><div class="rectangulo2"></div>Coronel Ramón Bernabé Estomba fue el fundador de la ciudad de Bahía Blanca
+        <br></br><div class="rectangulo2"></div>Actualmente tiene una cantidad de 335.190 habitantes.
+        <br></br><div class="rectangulo2"></div>Está ubicada en el sudoeste de la provincia de Buenos Aires, en el centro-este de la Argentina
+        <br></br><div class="rectangulo2"></div>La denominación de la localidad refiere a la profunda y angosta entrada de mar
+        en el continente<br></br>  y a laseflorescencias salinas blancas que cubren las zonas bajasen regiones costeras</p>
+      <br></br><br></br>
+      
+      <div class="rectangulo"> ㅤ</div>
 
-      <div class="rectangulo">.</div>
       <img class="imagen1" src="./img/bahiablanca.png" alt="Geo ubicación de el partido de Bahía blanca" />
 
+      <img class="imagen2" src="./img/bahiablancaa.jpg" alt="Geo ubicación de el partido de Bahía blanca" />
 
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./img/bahia1.jpg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="./img/bahia2.jpeg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="./img/bahia3.jpg" class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-      <div class="empleos">Con esta pagina web de la municipalidad de Bahía Blanca buscamos que las personas que necesiten un trabajo puedan hacerlo mediante nosotros. Porque VOS importas, para conocer más apreta el siguiente botón</div>
-
-
+      <p class="empleos">Con esta pagina web de la municipalidad de Bahía Blanca buscamos  que las personas que necesiten un trabajo puedan hacerlo mediante nosotros. Porque VOS importas, para conocer más apreta el siguiente botón</p>
 
       <button class="boton">Empleos disponibles</button>
 
