1139124af67b91b1058bdf748b2cfb056c0f06c8
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2023-11-06 17:26:08 -0300

    terminar cards

--- src/app/partials/trabajos/Trabajos.jsx
+++ src/app/partials/trabajos/Trabajos.jsx
@@ -5,30 +5,36 @@ export const Trabajos = () => {
         <>
             <div class="trabajos-container1">
                 <p class="trabajos">Obrero
+                <li class="datos">Empleador: Cooperativa Obrera Ltda</li>
                 <li class="datos">Descripcion del puesto:Trabajo disponible en<br></br> 
                 Rondeau 800, como operador de máquinas para<br></br>
                  trabajar la madera </li>
-                    <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                    <li class="datos">Rubro: Obra</li>
+                    <li class="datos">Requisitos: experiencia en operar máquinas</li>
+                    <li class="datos">Horario: 8hs Diarias (excepto domingos)</li>
                     <img class="imgtrabajos" src="./img/obrero.avif" />
                     <button class="botoncv">Enviar CV</button>
                 </p>
 
 
                 <p class="trabajos">Maestro
-                <li class="datos">Descripcion del puesto</li>
+                <li class="datos">Empleador: Escuela San Cayetano</li>
+                <li class="datos">Descripcion del puesto: Trabajo disponible en la<br></br>  
+                escuela San Cayetano como maestro de matemática</li>
                     <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                    <li class="datos">Requisitos: Titulo en el área de matemática</li>
+                    <li class="datos">Horario: 5hs de lunes a viernes</li>
                     <img class="imgtrabajos" src="./img/profe.png" />
                     <button class="botoncv">Enviar CV</button>
                 </p>
                 <p class="trabajos">Electricista
-                <li class="datos">Descripcion del puesto</li>
-                    <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                <li class="datos">Empleador: EDES</li>
+                <li class="datos">Descripcion del puesto: Trabajo disponible en la<br></br>
+                empresa electrica EDES</li>
+                    <li class="datos">Rubro: Obra</li>
+                    <li class="datos">Requisitos: Titulo en electricista y excasa <br></br>
+                    experiencia</li>
+                    <li class="datos">Horario: 4hs diarias (no incluye fin de semanas) </li>
                     <img class="imgtrabajos" src="./img/electricista.jpg" />
                     <button class="botoncv">Enviar CV</button>
                 </p></div>
@@ -36,26 +42,32 @@ export const Trabajos = () => {
 
             <div class="trabajos-container2">
                 <p class="trabajos">Peluquero
-                <li class="datos">Descripcion del puesto</li>
-                    <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                <li class="datos">Empleador: Vintage peluqueria</li>
+                <li class="datos">Descripcion del puesto: recepcionista en <br></br>
+                 peluqueria Vintage</li>
+                    <li class="datos">Rubro: Servicio</li>
+                    <li class="datos">Requisitos: Experiencia como recepcionista</li>
+                    <li class="datos">Horario: 6hs diarias</li>
                     <img class="imgtrabajos" src="./img/peluquero.jpg" />
                     <button class="botoncv">Enviar CV</button>
                 </p>
                 <p class="trabajos">Carpintero
-                <li class="datos">Descripcion del puesto</li>
-                    <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                <li class="datos">Empleador: Aaron González</li>
+                <li class="datos">Descripcion del puesto: Ayudante en <br></br>
+                Aaron Carpintería</li>
+                    <li class="datos">Rubro: Obra</li>
+                    <li class="datos">Requisitos: Titulo en carpintería</li>
+                    <li class="datos">Horario: 6hs de lunes a sabados</li>
                     <img class="imgtrabajos" src="./img/carpintero.webp" />
                     <button class="botoncv">Enviar CV</button>
                 </p>
                 <p class="trabajos">Enfermera
-                <li class="datos">Descripcion del puesto</li>
-                    <li class="datos">Rubro: Educación</li>
-                    <li class="datos">Requisitos</li>
-                    <li class="datos">Horario: 4HS</li>
+                <li class="datos">Empleador:Hospital Municipal de Agudos <br></br>
+                Dr. Leónidas Lucero</li>
+                <li class="datos">Descripcion del puesto: Ayudante en el hospital</li>
+                    <li class="datos">Rubro: Hospitalario</li>
+                    <li class="datos">Requisitos: Titulo en enfermería</li>
+                    <li class="datos">Horario: 7hs todos los días</li>
                     <img class="imgtrabajos" src="./img/enfermera.jpg" />
                     <button class="botoncv">Enviar CV</button>
                     </p>
