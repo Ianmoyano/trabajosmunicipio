1b17419c225c811b5198722f87808e3595821c26
JuanSuarez1 <juansuarez60249@gmail.com>
2023-11-06 19:36:10 -0300

    footer y boton trabajos

--- src/app/partials/footer/Footer.jsx
+++ src/app/partials/footer/Footer.jsx
@@ -6,9 +6,13 @@ export const Footer = () => {
         <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados
         <br></br>
         <img class="bandera" src="./img/bandera-de-bahia-blanca.jpg"  />
+       
+        <img class="face" src="./img/facebook.png"  />  <br></br><br></br>
+        <div class="facetext">Municipio Bahía Blanca</div>
         <img class="wasap" src="./img/wasap.png"/> <br></br><br></br>
-        <img class="wasap" src="./img/facebook.png"  />  <br></br><br></br>
-        <img class="maps" src="./img/maps.png"/>
+        <div class="wasaptext">+54-291-235-6732</div>
+        <img class="map" src="./img/maps.png"/>
+        <div class="mapstext">Bahía Blanca, Provincia de Buenos Aires</div>
 
         </footer>
     </>
--- src/app/partials/footer/Footer2.jsx
+++ src/app/partials/footer/Footer2.jsx
@@ -3,8 +3,18 @@ import React from "react";
 export const Footer2 = () => {
     return(
         <>
-        <footer class="Footer2">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados
-        <img class="escudo" src="./img/bahiaescudo.png"  /></footer>
+        <footer class="Footer">© 2023 Municipio oficial de Bahía blanca - Todos los derechos reservados
+        <br></br>
+        <img class="bandera" src="./img/bandera-de-bahia-blanca.jpg"  />
+       
+        <img class="face" src="./img/facebook.png"  />  <br></br><br></br>
+        <div class="facetext">Municipio Bahía Blanca</div>
+        <img class="wasap" src="./img/wasap.png"/> <br></br><br></br>
+        <div class="wasaptext">+54-291-235-6732</div>
+        <img class="map" src="./img/maps.png"/>
+        <div class="mapstext">Bahía Blanca, Provincia de Buenos Aires</div>
+
+        </footer>
     </>
     )
 }
