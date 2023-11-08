89c9ca7158fee2308d45c5bd202616be17727bf8
Ianmoyano <moyanoianlucca@gmail.com>
2023-11-08 18:02:03 -0300

    error boton

--- src/app/partials/titulo/Titulo.jsx
+++ src/app/partials/titulo/Titulo.jsx
@@ -1,13 +1,23 @@
+"use client"
 import React from "react";
+import { useRouter } from "next/navigation"
 
 export const Titulo = () => {
-    return(
+    const router = useRouter();
+  const navegarAEmpleos = () => {
+    router.push("empleos")
+  }
+  const navegarAMenu = () => {
+    router.push("")
+  }
+    return (
         <>
-        <img class="escudo" src="./img/bahiaescudo.png" ></img>
-        <div class="Titulo"></div><div class="titulo2">Municipalidad de Bahía Blanca</div>
-        
-        
-        
+            <img className="escudo" src="./img/bahiaescudo.png" ></img>
+            <div className="Titulo"></div><div class="titulo2">Municipalidad de Bahía Blanca</div>
+            
+            <button onClick={() => navegarAMenu()} className="boton2">Menu</button> 
+            <button onClick={() => navegarAEmpleos()} className="boton2">Empleos</button> 
+
         </>
     )
 }
