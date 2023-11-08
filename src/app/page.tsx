8d9883da747cd4afc849091477cae3d01c32f357
Ianmoyano <moyanoianlucca@gmail.com>
2023-11-08 16:41:40 -0300

    arreglo de error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home()  {
   return (<>
 
 <Titulo/>
-<Trabajos/>
-<Footer2/>
+<Main/>
+<Footer/>
     
  </>
   )
--- src/app/partials/main/Main.jsx
+++ src/app/partials/main/Main.jsx
@@ -4,12 +4,12 @@ export const Main = () => {
   return(<><div>
     
       <br />
-      <p class="infociudad"><div class="rectangulo3"> ㅤ</div><div class="rectangulo2"></div>Bahía Blanca es una ciudad que se fundo en 1828.
+      <div class="infociudad"><div class="rectangulo3"> ㅤ</div><div class="rectangulo2"></div>Bahía Blanca es una ciudad que se fundo en 1828.
         <br></br><div class="rectangulo2"></div>Coronel Ramón Bernabé Estomba fue el fundador de la ciudad de Bahía Blanca
         <br></br><div class="rectangulo2"></div>Actualmente tiene una cantidad de 335.190 habitantes.
         <br></br><div class="rectangulo2"></div>Está ubicada en el sudoeste de la provincia de Buenos Aires, en el centro-este de la Argentina
         <br></br><div class="rectangulo2"></div>La denominación de la localidad refiere a la profunda y angosta entrada de mar
-        en el continente<br></br>  y a laseflorescencias salinas blancas que cubren las zonas bajasen regiones costeras</p>
+        en el continente<br></br>  y a laseflorescencias salinas blancas que cubren las zonas bajasen regiones costeras</div>
       <br></br><br></br>
       
       <div class="rectangulo"> ㅤ</div>
