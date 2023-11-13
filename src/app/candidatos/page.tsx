045468d043e2441c7fc6c77139dee570a2156420
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2023-11-13 19:42:12 -0300

    diseño cv

--- src/app/candidatos/page.tsx
+++ src/app/candidatos/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { Titulo } from "../partials/titulo/titulo"
-
+import { CV } from "../partials/curriculum/CV"
 
 export default function Home() {
   return (<>
     <Titulo />
+    <CV />
   </>
   )
 }
--- src/app/partials/curriculum/CV.jsx
+++ src/app/partials/curriculum/CV.jsx
@@ -11,11 +11,11 @@ export const CV = () => {
   }
 
   return ( <>
-    <div>
-      <h2>Introduce tu CV aquí</h2>
+    <div className="englobador">
+      <div className="cv">Introduce tu CV aquí</div><br></br>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Nombre</label>
+        <div className="nombre">
+          <label>Nombre:</label><br></br>
           <input type="text" {...register('nombre', {                       
               required: true,
               minLength: 3,
@@ -26,8 +26,8 @@ export const CV = () => {
           {errors.nombre?.type === "minLength" && <p>El nombre debe tener al menos 3 letras</p>}
           {errors.apellido?.type === "maxLength" && <p>El nombre no debe superar las 15 letras</p>}
         </div>
-        <div>
-          <label>Apellido</label>
+        <div className="apellidos">
+          <label>Apellido:</label><br></br>
           <input type="text" {...register('apellido', {
             required: true,
             minLength: 3,
@@ -38,8 +38,8 @@ export const CV = () => {
           {errors.apellido?.type === "minLength" && <p>El apellido debe tener al menos 3 letras</p>}
           {errors.apellido?.type === "maxLength" && <p>El apellido no debe superar las 15 letras</p>}
         </div>
-        <div>
-          <label>Edad</label>
+        <div className="edad">
+          <label>Edad:</label><br></br>
           <input type="number" {...register('edad', {
             required: true,
             maxLength: 2,
@@ -48,8 +48,8 @@ export const CV = () => {
           {errors.edad?.type === "required" && <p>La edad es obligatoria</p>}
           {errors.edad && <p>La edad debe estar entre 18 y 65</p>}
         </div>
-        <div>
-          <label>Mail</label>
+        <div className="mail">
+          <label>Mail:</label><br></br>
           <input type="text" {...register('mail',{
            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
            required:true
@@ -57,13 +57,13 @@ export const CV = () => {
            {errors.mail?.type === "required" && <p>El mail es obligatorio</p>}
            {errors.mail?.type === "pattern" && <p>El formato del mail es erróneo</p>}
            </div>
-        <div>
-          <label>Full time</label>
+        <div className="fulltime">
+          <label>Full time:</label><br></br>
           <select {...register('fulltime')}>
             <option value="si">Si</option>
             <option value="no">No</option>
           </select>
-        </div>
+        </div><br></br>
         <input type="submit" value="Enviar CV" />
       </form>
     </div>
