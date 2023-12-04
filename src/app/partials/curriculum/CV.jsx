3d16b2ff3f309439ad5bcd16b319473f20a94c2b
JuanSuarez1 <juansuarez60249@gmail.com>
2023-12-04 02:46:30 -0300

    filtro en trabajos y css acomodado

--- src/app/partials/curriculum/CV.jsx
+++ src/app/partials/curriculum/CV.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+import React, {useState} from "react";
 import { useForm } from "react-hook-form";
 import { edadValidator } from "./validator.js";
 import Card from 'react-bootstrap/Card';
@@ -8,14 +8,42 @@ import datos from "../candidatosdisp.json";
 
 export const CV = () => {
   const { register, formState: { errors }, handleSubmit } = useForm();
+  const [fulltimeSeleccionadas, SetFulltimeSeleccionadas] = useState(''); 
+  const [movilidadSeleccionadas, SetMovilidadSeleccionadas] = useState(''); 
 
   const onSubmit = (data) => {
     console.log(data);
-  }
+  };
+  const handleFulltimeChange = (event) => {
+    SetFulltimeSeleccionadas(event.target.value);
+    
+  };
+
+  const handleMovilidadChange = (event) => {
+    SetMovilidadSeleccionadas(event.target.value);
+  };
+  const candidatosFiltrados = datos.filter((item) => {
+    const filtroFulltime = !fulltimeSeleccionadas || item.fullTime === fulltimeSeleccionadas;
+    const filtroMovilidad = !movilidadSeleccionadas || item.movilidad === movilidadSeleccionadas;
+    return filtroFulltime && filtroMovilidad;
+  });
 
   return (<>
+      <div className="filtrarcandidatos">
+        <select value={fulltimeSeleccionadas} onChange={handleFulltimeChange}>
+          <option value="">Filtrar por fulltime</option>
+          <option>Si</option>
+          <option>No</option>
+        </select>
+        <select value={movilidadSeleccionadas} onChange={handleMovilidadChange}>
+          <option value="">Filtrar por movilidad</option>
+          <option>Si</option>
+          <option>No</option>
+        </select>
+      </div>
+
     <Container className="cardscandi">
-      {datos.map((item) => (
+    {candidatosFiltrados.map((item)=>(
         <Card key={item.nombre} style={{ width: '30rem' }}>
           <Card.Body>
             <Card.Title>{item.nombre}</Card.Title>
@@ -38,19 +66,18 @@ export const CV = () => {
     <div className="englobador">
       <div className="cv">Introduce tu CV aquí</div><br></br>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="nombre">
+        <div className="labels">
           <label>Nombre:</label><br></br>
           <input type="text" {...register('nombre', {
             required: true,
             minLength: 3,
           })}
           />
-          <div className="errores1">
+          <div className="errores">
             {errors.nombre?.type === "required" && <p>El nombre es obligatorio.</p>}
             {errors.nombre?.type === "minLength" && <p>El nombre debe tener al menos 3 letras.</p>}
           </div>
-        </div>
-        <div className="apellidos">
+        <div className="">
           <label>Apellido:</label><br></br>
           <input type="text" {...register('apellido', {
             required: true,
@@ -58,52 +85,52 @@ export const CV = () => {
            
           })}
           />
-          <div className="errores2">
+          <div className="errores">
             {errors.apellido?.type === "required" && <p>El apellido es obligatorio.</p>}
             {errors.apellido?.type === "minLength" && <p>El apellido debe tener al menos 3 letras.</p>}
           </div>
         </div>
-        <div className="edad">
+        <div className="">
           <label>Edad:</label><br></br>
           <input type="number" {...register('edad', {
             required: true,
             maxLength: 2,
             validate: edadValidator
           })} />
-          <div className="errores3">
+          <div className="errores">
             {errors.edad?.type === "required" && <p>La edad es obligatoria.</p>}
             {errors.edad?.type === "validate" && <p>La edad tiene que ser entre 18 y 65 años.</p>}
           </div>
         </div>
-        <div className="Habilidades">
+        <div className="">
           <label> Habilidades(max 5):</label><br></br>
           <input type="text" {...register('habilidades', {
             required: true,
             mixLength: 20,
           })} />
-          <div className="errores4">
+          <div className="errores">
             {errors.habilidades?.type === "required" && <p>Las habilidades son obligatorias. </p>}
           </div>
         </div>
-        <div className="mail">
+        <div className="">
           <label>Mail:</label><br></br>
           <input type="text" {...register('mail', {
             pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
             required: true
           })} />
-          <div className="errores5">
+          <div className="errores">
             {errors.mail?.type === "required" && <p>El mail es obligatorio.</p>}
             {errors.mail?.type === "pattern" && <p>El formato del mail es erróneo.</p>}
           </div>
         </div>
-        <div className="fulltime">
+        <div className="">
           <label>Full time:</label><br></br>
           <select {...register('fulltime')}>
             <option value="si">Si</option>
             <option value="no">No</option>
           </select>
         </div>
-        <div className="movilidad">
+        <div className="">
           <label>Movilidad:</label><br></br>
           <select {...register('movilidad')}>
             <option value="si">Si</option>
@@ -111,10 +138,11 @@ export const CV = () => {
           </select>
         </div><br></br>
         <input className="submit" type="submit" value="Enviar CV" />
-
+        </div>
       </form>
     </div>
-    <div className="rectangulo6"></div>
+    
+    <div className="rectangulo"></div>
   </>
   );
 }
--- src/app/partials/trabajos/Trabajos.jsx
+++ src/app/partials/trabajos/Trabajos.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import { useRouter } from "next/navigation"
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { Container } from "reactstrap";
-import datos from "../empleosdisp.json";
+import { Container } from 'reactstrap';
+import datos from '../empleosdisp.json';
 
 export const Trabajos = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const [horasSeleccionadas, setHorasSeleccionadas] = useState(''); 
+  const [rubroSeleccionado, setRubroSeleccionado] = useState('');
 
-    const navegarACandidatos = () => {
-        router.push("candidatos")
-        console.log(datos);
-    }
+  const navegarACandidatos = () => {
+    router.push('candidatos');
+  };
 
-    return (
-        <>
-            <h2 className="subtitulotrabajos">Trabajos disponibles :</h2>
-            <button onClick={() => navegarACandidatos()} className="buscatrab">Personas en busca de trabajo</button>
+  const handleHorasChange = (event) => {
+    setHorasSeleccionadas(event.target.value);
+    
+  };
 
-            <Container className="cardsempleos">
-                {datos.map((item) => (
-                    <Card key={item.trabajo} style={{ width: '19rem', height: '34rem' }}>
+  const handleRubroChange = (event) => {
+    setRubroSeleccionado(event.target.value);
+  };
+    const trabajosFiltrados = datos.filter((item) => {
+        const filtroHoras = !horasSeleccionadas || parseInt(item.horario) === parseInt(horasSeleccionadas, 10);
+        const filtroRubro = !rubroSeleccionado || item.rubro === rubroSeleccionado;
+        return filtroHoras && filtroRubro;
+      });
+
+  return (
+    <>
+      <h2 className="subtitulotrabajos">Trabajos disponibles :</h2>
+      <button onClick={() => navegarACandidatos()} className="buscatrab">
+        Personas en busca de trabajo
+      </button>
+      <div className="filtrartrabajos">
+        <select value={horasSeleccionadas} onChange={handleHorasChange}>
+          <option value="">Filtrar por horas</option>
+          <option>4</option>
+          <option>8</option>
+        </select>
+        <select value={rubroSeleccionado} onChange={handleRubroChange}>
+          <option value="">Filtrar por rubro</option>
+          <option>Obra</option>
+          <option>Servicio</option>
+        </select>
+      </div>
+
+      <Container className="cardsempleos">
+      {trabajosFiltrados.map((item) => (
+          <Card key={item.trabajo} style={{ width: '19rem', height: '34rem' }}>
                         <Card.Img className="cardfoto" variant="top" src={item.foto} />
                         <Card.Body>
                             <Card.Title>{item.trabajo}</Card.Title>
@@ -29,7 +58,7 @@ export const Trabajos = () => {
                                     <p>_ 𝘿𝙚𝙨𝙘𝙧𝙞𝙥𝙘𝙞𝙤́𝙣: {item.descripcion}</p>
                                     <p>_ 𝙍𝙪𝙗𝙧𝙤: {item.rubro}</p>
                                     <p>_ 𝙍𝙚𝙦𝙪𝙞𝙨𝙞𝙩𝙤𝙨: {item.requisitos}</p>
-                                    <p>_ 𝙃𝙤𝙧𝙖𝙧𝙞𝙤: {item.horario} , horas diarias</p>
+                                    <p>_ 𝙃𝙤𝙧𝙖𝙧𝙞𝙤: {item.horario}, horas diarias</p>
                                 </div>
                             <Button className="enviarcv" onClick={() => navegarACandidatos()} variant="primary">Enviar cv</Button>
                         </Card.Body>
@@ -38,5 +67,4 @@ export const Trabajos = () => {
             </Container>
         </>
     );
-};
-
+}
