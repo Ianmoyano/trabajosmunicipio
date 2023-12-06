65b82c841b986fc4019a375bf11ec5a1119eefdf
JuanSuarez1 <juansuarez60249@gmail.com>
2023-12-06 01:35:31 -0300

    error cv

--- src/app/partials/curriculum/CV.jsx
+++ src/app/partials/curriculum/CV.jsx
@@ -48,18 +48,18 @@ export const CV = () => {
 
   return (<>
       <div className="filtrarcandidatos">
-        <input value={nombreSeleccionado} onChange={handleNombreChange} placeholder="Buscar por nombre..."></input>
-        <select value={fulltimeSeleccionadas} onChange={handleFulltimeChange}>
+        <input id="nombreInput" value={nombreSeleccionado} onChange={handleNombreChange} placeholder="Buscar por nombre..."></input>
+        <select id="fulltimeSelect" value={fulltimeSeleccionadas} onChange={handleFulltimeChange}>
           <option value="">Filtrar por fulltime</option>
           <option>Si</option>
           <option>No</option>
         </select>
-        <select value={movilidadSeleccionada} onChange={handleMovilidadChange}>
+        <select id="movilidadSelect" value={movilidadSeleccionada} onChange={handleMovilidadChange}>
           <option value="">Filtrar por movilidad</option>
           <option>Si</option>
           <option>No</option>
         </select>
-        <select value={edadSeleccionada} onChange={handleEdadChange}>
+        <select  id="edadSelect" value={edadSeleccionada} onChange={handleEdadChange}>
           <option value="">Filtrar por edad</option>
           <option>18-29</option>
           <option>30-49</option>
@@ -81,7 +81,7 @@ export const CV = () => {
                   <li> Cualidades: {item.cualidades}.</li>
                   </ul>
               </div>
-            <Card.Link href="#">Curriculum Vitae</Card.Link>
+            <Card.Link href="https://ar.linkedin.com">Curriculum Vitae</Card.Link>
           </Card.Body>
         </Card>
       ))}
@@ -92,75 +92,75 @@ export const CV = () => {
       <div className="cv">Introduce tu CV aquí</div><br></br>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="labels">
-          <label>Nombre:</label><br></br>
+          <label>Nombre:<br></br>
           <input type="text" {...register('nombre', {
             required: true,
             minLength: 3,
           })}
-          />
-          <div className="errores">
+          /></label>
+          <div className="error1">
             {errors.nombre?.type === "required" && <p>El nombre es obligatorio.</p>}
             {errors.nombre?.type === "minLength" && <p>El nombre debe tener al menos 3 letras.</p>}
           </div>
         <div className="">
-          <label>Apellido:</label><br></br>
+          <label>Apellido:<br></br>
           <input type="text" {...register('apellido', {
             required: true,
             minLength: 3,
            
           })}
-          />
-          <div className="errores">
+          /></label>
+          <div className="error2">
             {errors.apellido?.type === "required" && <p>El apellido es obligatorio.</p>}
             {errors.apellido?.type === "minLength" && <p>El apellido debe tener al menos 3 letras.</p>}
           </div>
         </div>
         <div className="">
-          <label>Edad:</label><br></br>
+          <label>Edad:<br></br>
           <input type="number" {...register('edad', {
             required: true,
             maxLength: 2,
             validate: edadValidator
-          })} />
-          <div className="errores">
+          })} /></label>
+          <div className="error3">
             {errors.edad?.type === "required" && <p>La edad es obligatoria.</p>}
             {errors.edad?.type === "validate" && <p>La edad tiene que ser entre 18 y 65 años.</p>}
           </div>
         </div>
         <div className="">
-          <label> Habilidades(max 5):</label><br></br>
+          <label> Habilidades(max 5):<br></br>
           <input type="text" {...register('habilidades', {
             required: true,
             mixLength: 20,
-          })} />
-          <div className="errores">
+          })} /></label>
+          <div className="error4">
             {errors.habilidades?.type === "required" && <p>Las habilidades son obligatorias. </p>}
           </div>
         </div>
         <div className="">
-          <label>Mail:</label><br></br>
+          <label>Mail:<br></br>
           <input type="text" {...register('mail', {
             pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
             required: true
-          })} />
-          <div className="errores">
+          })} /></label>
+          <div className="error5">
             {errors.mail?.type === "required" && <p>El mail es obligatorio.</p>}
             {errors.mail?.type === "pattern" && <p>El formato del mail es erróneo.</p>}
           </div>
         </div>
         <div className="">
-          <label>Full time:</label><br></br>
+          <label>Full time:<br></br>
           <select {...register('fulltime')}>
             <option value="si">Si</option>
             <option value="no">No</option>
-          </select>
+          </select></label>
         </div>
         <div className="">
-          <label>Movilidad:</label><br></br>
+          <label>Movilidad:<br></br>
           <select {...register('movilidad')}>
             <option value="si">Si</option>
             <option value="no">No</option>
-          </select>
+          </select></label>
         </div><br></br>
         <input className="submit" type="submit" value="Enviar CV" />
         </div>
