a730aae8dda06714a793261588788028b1b868ac
JuanSuarez1 <juansuarez60249@gmail.com>
2023-12-06 00:42:08 -0300

    Main actualizado

--- src/app/partials/main/Main.jsx
+++ src/app/partials/main/Main.jsx
@@ -8,24 +8,16 @@ export const Main = () => {
     router.push("empleos")
   }
   return (<>
-  <div className="infociudad"> <div className="rectangulo2"></div>La ciudad de Bahía Blanca se fundo en 1828.
-      <br></br><div className="rectangulo2"></div>Coronel Ramón Bernabé Estomba fue el fundador de la ciudad de Bahía Blanca
-      <br></br><div className="rectangulo2"></div>Actualmente la ciudad tiene una cantidad de 335.190 habitantes.
-      <br></br><div className="rectangulo2"></div>Está ubicada en el sudoeste de la provincia de Buenos Aires, en el centro-este de la Argentina
-      <br></br><div className="rectangulo2"></div>La denominación de la localidad refiere a la profunda y angosta entrada de mar
-      en el continente y a las<br></br> florescencias salinas blancas que cubren las zonas bajasen regiones costeras
+  <h2 className="subtitulo">¡Te damos la bienvenida a Bahía Blanca!</h2>
+  <div className="infociudad">
+  Ubicada en el sur de la provincia de Buenos Aires, Bahía Blanca es un enclave donde la rica historia se entrelaza con la modernidad. Sus calles narran décadas de historias, mientras que a orillas del Atlántico, las playas ofrecen una serenidad inigualable. Con un activo puerto que impulsa la economía, la ciudad refleja una autenticidad palpable y la calidez de su comunidad. Explora Bahía Blanca, un lugar donde la tradición y la vitalidad contemporánea convergen en un entorno único. ¡Descubre nuestra ciudad, descubre Bahía Blanca!
    </div>
-
-  
-
-    
-        <img className="imagen1" src="./img/bahiablanca.png" alt="Geo ubicación de el partido de Bahía blanca" />
-
+   
+    <img className="imagen1" src="./img/bahiablanca.png" alt="Geo ubicación de el partido de Bahía blanca" />
     <img className="imagen2" src="./img/bahiablancaa.jpg" alt="Geo ubicación de el partido de Bahía blanca" />
-
     <div className="empleos">Con esta pagina web de la municipalidad de Bahía Blanca buscamos  que las personas que necesiten un trabajo puedan hacerlo mediante nosotros. Porque VOS importas, para conocer más apreta el siguiente botón
-    
     </div>
+    
 <button onClick={() => navegarAEmpleos()} className="boton">Empleos disponibles</button> 
 <div className="linea2"></div>
 <div className="MunicipiodeBahia">Municipio de Bahia</div>
